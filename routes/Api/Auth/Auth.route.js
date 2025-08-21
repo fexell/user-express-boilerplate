203757af78bfa402a608cbfc76ce54bdc5c08e1f
Felix Cervin <fexellofficial@gmail.com>
2025-08-21 19:00:39 +0200

    fix: Added the AlreadyLoggedOut middleware to the /logout route

--- routes/Api/Auth/Auth.route.js
+++ routes/Api/Auth/Auth.route.js
@@ -7,7 +7,7 @@ import AuthMiddleware from '../../../middlewares/Auth.middleware.js'
 const AuthRouter                            = Router()
 
 AuthRouter.post( '/login', [ AuthMiddleware.AlreadyLoggedIn ], AuthController.Login )
-AuthRouter.post( '/logout', AuthController.Logout )
+AuthRouter.post( '/logout', [ AuthMiddleware.AlreadyLoggedOut ], AuthController.Logout )
 
 export {
   AuthRouter as default,
