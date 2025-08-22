45028b9c5ec2ecf888b58cff5c017462fa58e081
Felix Cervin <fexellofficial@gmail.com>
2025-08-22 20:11:06 +0200

    refactor & docs: Commented most of User.controller.js and refactored GetUser() method a bit

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -6,9 +6,27 @@ import CustomErrorHelper from '../helpers/Error.helper.js'
 import ResponseHelper from '../helpers/Response.helper.js'
 import UserHelper from '../helpers/User.helper.js'
 
+/**
+ * @class UserController
+ * @classdesc Contains all controller methods related to the user
+ * 
+ * @method UserController.Create - Create a new user
+ * @method UserController.GetUser - Get the user's details
+ */
 class UserController {
+
+  /**
+   * @method UserController.Create
+   * @description The controller method handling creating a new user (most of the validation is done in the user model, User.model.js)
+   * @param {Request} req 
+   * @param {Response} res 
+   * @param {NextFunction} next 
+   * @returns 
+   */
   static async Create( req, res, next ) {
     try {
+
+      // Destructure the request body
       const {
         email,
         username,
@@ -18,9 +36,11 @@ class UserController {
         passwordConfirm,
       }                                     = req.body
 
+      // If the passwords don't match
       if(password !== passwordConfirm)
         throw new CustomErrorHelper( req.t('password.mismatch') )
 
+      // Create the new user
       const newUser                         = new UserModel({
         email,
         username,
@@ -29,8 +49,10 @@ class UserController {
         password,
       })
 
+      // Save the new user
       await newUser.save()
 
+      // Return the new user
       return ResponseHelper.Success( res, req.t('user.created'), UserModel.SerializeUser( newUser ), 'user' )
 
     } catch ( error ) {
@@ -38,11 +60,25 @@ class UserController {
     }
   }
 
+  /**
+   * @method UserController.GetUser
+   * @description The controller method handling getting the _current_ user's details
+   * @param {Request} req 
+   * @param {Response} res 
+   * @param {NextFunction} next 
+   * @returns 
+   */
   static async GetUser( req, res, next ) {
     try {
-      const userId                          = UserHelper.GetUserId( req, res )
-      const user                            = await UserHelper.GetUserById( userId, true )
 
+      // Retrieve the user by user id
+      const user                            = await UserHelper.GetUserById( UserHelper.GetUserId( req, res ), true )
+
+      // If the user doesn't exist
+      if( !user )
+        throw new CustomErrorHelper( req.t('user.notFound') )
+
+      // Return the user
       return ResponseHelper.Success( res, req.t('user.found'), UserModel.SerializeUser( user ), 'user' )
 
     } catch ( error ) {
